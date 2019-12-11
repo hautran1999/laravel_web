bafbe3d00aef9dc1417042904d7ad1e933726458
hautran1999 <43260685+hautran1999@users.noreply.github.com>
2019-12-11 19:13:14 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,7 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {   
         
-        \URL::forceScheme('https');
+        
+        URL::forceScheme('https');
         //
     }
 }
