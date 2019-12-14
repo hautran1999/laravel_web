8b2f9035448e47afdf9c5c7430043ecbc9a54e15
hautran1999 <43260685+hautran1999@users.noreply.github.com>
2019-12-14 17:30:09 +0700

    Update myExam.blade.php

--- resources/views/myExam.blade.php
+++ resources/views/myExam.blade.php
@@ -23,10 +23,11 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm container your-running" style="cursor:pointer" onclick="">
+                    <div class="card mb-4 shadow-sm container your-running" style="cursor:pointer"
+                        onclick="showRunning()">
                         <div class="row">
                             <div class="col-sm-6 card-content">
-                                <h1>0</h1>
+                                <h1>{{count($exam_running)}}</h1>
                                 <h3>Running</h3>
                             </div>
                             <div class="col-sm-6 card-content"><i class="fa fa-rocket icon-size"></i></div>
@@ -69,7 +70,7 @@
                             <th scope="col">Subject</th>
                             <th style="width:250px;" scope="col">Describe</th>
                             <th scope="col">Create Date</th>
-                            <th style="width:350px;" scope="col">Handle</th>
+                            <th style="width:400px;" scope="col">Handle</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -84,25 +85,57 @@
                             <td>{{$ex->exam_describe}}</td>
                             <td>{{$ex->created_at}}</td>
                             <td>
-                                <a href="{{route('pass_exam',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-success"><span><i class="fa fa-graduation-cap"></i></span>
-                                    Test</a>
-                                <a href="{{route('info',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-info"><span><i class="fa fa-bar-chart"></i></span>
-                                    Info</a>
-                                <a href="{{route('edit',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-primary"><span><i class="fa fa-edit"></i></span>
-                                    Edit Exam</a>
-                                <a href="{{route('edit_info',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-primary"><span><i class="fa fa-edit"></i></span>
-                                    Edit Info</a>
-                                <a href="{{route('edit',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-primary"><span><i class="fa fa-edit"></i></span>
-                                    Off</a>
-                                <a href="{{route('delete_exam',$ex->exam_id)}}" class="btn btn-danger"><span><i
-                                            class="fa fa-trash-o"></i></span>
-                                    Delete</a>
-
+                                <div class="row">
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('pass_exam',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-success"><i
+                                                    class="fa fa-graduation-cap"></i></button><br>
+                                            <p class="text-center">Test</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('info',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-info"><i
+                                                    class="fa fa-bar-chart"></i></button><br>
+                                            <p class="text-center">Info</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('edit',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-primary"><i
+                                                    class="fa fa-edit"></i></button><br>
+                                            <p class="text-center">EditExam</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('edit_info',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-warning"><i
+                                                    class="fa fa-pencil"></i></button><br>
+                                            <p class="text-center">EditInfo</p>
+                                        </a>
+                                    </div>
+                                    @if($ex->running==0)
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('run_exam',$ex->exam_id)}}"><button type="button"
+                                                class="btn btn-secondary"><i class="fa fa-play"></i></button><br>
+                                            <p class="text-center">Run</p>
+                                        </a>
+                                    </div>
+                                    @else
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('stop_exam',$ex->exam_id)}}"><button type="button"
+                                                class="btn btn-secondary"><i class="fa fa-play"></i></button><br>
+                                            <p class="text-center">Stop</p>
+                                        </a>
+                                    </div>
+                                    @endif
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('delete_exam',$ex->exam_id)}}"><button type="button"
+                                                class="btn btn-danger"><i class="fa fa-trash-o"></i></button><br>
+                                            <p class="text-center">Delete</p>
+                                        </a>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                         @endforeach
@@ -166,6 +199,93 @@
             </div>
         </div>
     </div>
+    <div id="running">
+        <div class="grid">
+            <div class="grid-body">
+                <div>
+                    <h3 class="d-inline">My exam Running</h3>
+                    <form class="form-inline my-2 my-lg-0 search-form">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="grid">
+            <div class="grid-body">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th style="width:150px;" scope="col">Exam</th>
+                            <th scope="col">Subject</th>
+                            <th style="width:250px;" scope="col">Describe</th>
+                            <th scope="col">Create Date</th>
+                            <th style="width:400px;" scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($exam_running as $ex)
+                        <tr>
+                            <th scope="row">{{$run++}}</th>
+                            <td>
+                                <span>{{$ex->exam_name}}</span><br>
+                                <small>{{$ex->name}}</small>
+                            </td>
+                            <td>{{$ex->exam_kind}}</td>
+                            <td>{{$ex->exam_describe}}</td>
+                            <td>{{$ex->created_at}}</td>
+                            <td>
+                                <div class="row">
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('pass_exam',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-success"><i
+                                                    class="fa fa-graduation-cap"></i></button><br>
+                                            <p class="text-center">Test</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('info',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-info"><i
+                                                    class="fa fa-bar-chart"></i></button><br>
+                                            <p class="text-center">Info</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('edit',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-primary"><i
+                                                    class="fa fa-edit"></i></button><br>
+                                            <p class="text-center">EditExam</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('edit_info',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-warning"><i
+                                                    class="fa fa-pencil"></i></button><br>
+                                            <p class="text-center">EditInfo</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('stop_exam',$ex->exam_id)}}"><button type="button"
+                                                class="btn btn-secondary"><i class="fa fa-play"></i></button><br>
+                                            <p class="text-center">Stop</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-2 text-center">
+                                        <a href="{{route('delete_exam',$ex->exam_id)}}"><button type="button"
+                                                class="btn btn-danger"><i class="fa fa-trash-o"></i></button><br>
+                                            <p class="text-center">Delete</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     <div id="join">
         <div class="grid">
             <div class="grid-body">
@@ -184,9 +304,10 @@
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">Exam</th>
+                            <th style="width:200px;" scope="col">Exam</th>
                             <th scope="col">Subject</th>
-                            <th style="width: 400px" scope="col">Describe</th>
+                            <th style="width:300px;" scope="col">Describe</th>
+                            <th scope="col">Create Date</th>
                             <th scope="col">Handle</th>
                         </tr>
                     </thead>
@@ -200,17 +321,33 @@
                             </td>
                             <td>{{$ex->exam_kind}}</td>
                             <td>{{$ex->exam_describe}}</td>
+                            <td>{{$ex->created_at}}</td>
 
                             <td>
-                                <a href="{{route('pass_exam',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-success"><span><i class="fa fa-graduation-cap"></i></span>
-                                    Test</a>
-                                <a href="{{route('info_exam',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-info"><span><i class="fa fa-bar-chart"></i></span>
-                                    Info</a>
-                                <a href="{{route('report',$ex->exam_name.'&&'.$ex->exam_id)}}"
-                                    class="btn btn-danger"><span><i class="fa fa-exclamation-circle"></i></span>
-                                    Report</a>
+                                <div class="row">
+                                    <div class="col-sm-4 text-center">
+                                        <a href="{{route('pass_exam',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-success"><i
+                                                    class="fa fa-graduation-cap"></i></button><br>
+                                            <p class="text-center">Test</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-4 text-center">
+                                        <a href="{{route('info_exam',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-info"><i
+                                                    class="fa fa-bar-chart"></i></button><br>
+                                            <p class="text-center">Info</p>
+                                        </a>
+                                    </div>
+                                    <div class="col-sm-4 text-center">
+                                        <a href="{{route('report',$ex->exam_name.'&&'.$ex->exam_id)}}"><button
+                                                type="button" class="btn btn-danger"><i
+                                                    class="fa fa-exclamation-circle"></i></button><br>
+                                            <p class="text-center">Report</p>
+                                        </a>
+                                    </div>
+                                </div>
+
                             </td>
                         </tr>
                         @endforeach
@@ -229,13 +366,21 @@
             }
         }
     function showJoin(){
+        $('#running').hide();
         $('#created').hide();
         $('#join').show();
         //$('#created').hide();
     }
     function showCreate(){
+        $('#running').hide();
         $('#join').hide();
         $('#created').show();
+        
+    }
+    function showRunning(){
+        $('#join').hide();
+        $('#created').hide();
+        $('#running').show();
     }
     document.addEventListener('DOMContentLoaded', function() {
         showCreate();
